a6d436cf1cb8483d64201f53fe163f2023754a00
di-i <55539864+di-i@users.noreply.github.com>
2020-08-11 16:26:32 +0300

    Deleted changes for fa-icons

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -15,7 +15,7 @@
     $('#sidebar-wrapper .js-scroll-trigger').click(function() {
       $("#sidebar-wrapper").removeClass("active");
       $(".menu-toggle").removeClass("active");
-      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+      
     });
   
   
